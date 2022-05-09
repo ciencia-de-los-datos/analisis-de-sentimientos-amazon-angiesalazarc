f7556eed988cf38e9c39180fa7c586e1cabfaadf
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-05-08 19:34:28 -0500

    Lectura de Datos

--- preguntas.py
+++ preguntas.py
@@ -21,11 +21,11 @@ def pregunta_01():
     # Lea el archivo `amazon_cells_labelled.tsv` y cree un DataFrame usando pandas.
     # Etiquete la primera columna como `msg` y la segunda como `lbl`. Esta función
     # retorna el dataframe con las dos columnas.
-    df = ____(
-        ____,
-        sep=____,
-        header=____,
-        names=____,
+    df = pd.read_csv(
+        "amazon_cells_labelled.tsv",
+        sep="\t",
+        header=None,
+        names=["msg","lbl"],
     )
 
     # Separe los grupos de mensajes etiquetados y no etiquetados.
